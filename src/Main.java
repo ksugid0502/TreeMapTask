5eff6aade6a288f22085a37b661a18f583d79efe
Ksenia <suhodoeva.ksenia@yandex.ru>
2023-06-09 16:32:48 +0400

    Task completed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(new Person("Anthony", "Steven Evans", 33));
+        personList.add(new Person("Nathan", "Stewart", 20));
+        personList.add(new Person("Jose", "Jonathan Gonzalez Richardson", 35));
+        personList.add(new Person("Anthony", "Dylan Sanders", 41));
+        personList.add(new Person("Charles", "Richardson", 25));
+
+        Collections.sort(personList, new PersonComparator(3));
+
+        for(Person p: personList) {
+            System.out.println(p.toString());
+        }
+
     }
 }
