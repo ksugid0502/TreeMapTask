9ae3d12495a23c4cd9aaed6711574f1cc45518f6
Ksenia <suhodoeva.ksenia@yandex.ru>
2023-06-10 19:01:44 +0400

    lambda eraser

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,15 @@ public class Main {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<Person>();
         personList.add(new Person("Anthony", "Steven Evans", 33));
-        personList.add(new Person("Nathan", "Stewart", 20));
+        personList.add(new Person("Nathan", "Stewart", 16));
         personList.add(new Person("Jose", "Jonathan Gonzalez Richardson", 35));
-        personList.add(new Person("Anthony", "Dylan Sanders", 41));
+        personList.add(new Person("Anthony", "Dylan Sanders", 17));
         personList.add(new Person("Charles", "Richardson", 25));
 
-        Collections.sort(personList, new PersonComparator(3));
+        personList.removeIf(p -> p.getAge() < 18);
 
         for(Person p: personList) {
             System.out.println(p.toString());
         }
-
     }
 }
